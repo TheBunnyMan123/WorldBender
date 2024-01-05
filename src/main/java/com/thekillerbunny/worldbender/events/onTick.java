5bd6decabd7bb8dcfc18793102b521e8bf08f4ab
TheKillerBunny <tegguemcnair+git@gmail.com>
2024-01-05 01:23:30 +0000

    fix oopsie

---
+++ src/main/java/com/thekillerbunny/worldbender/events/onTick.java
@@ -0,0 +1,22 @@
+package com.thekillerbunny.worldbender.events;
+
+import com.thekillerbunny.worldbender.commandQueue;
+import com.thekillerbunny.worldbender.worldBender;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents.EndTick;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+
+public class onTick implements EndTick {
+    @Override
+    public void onEndTick(MinecraftClient client) {
+        ClientPlayNetworkHandler networkHandler = client.getNetworkHandler();
+        try {
+            networkHandler.getServerInfo();
+        }catch(java.lang.NullPointerException e) {
+            commandQueue.clearQueue();
+            com.thekillerbunny.worldbender.worldBender.positionsSet[0] = false;
+            com.thekillerbunny.worldbender.worldBender.positionsSet[1] = false;
+        }
+    }
+}
